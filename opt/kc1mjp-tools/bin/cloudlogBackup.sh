bb74241c7c5556ec47002f580d51a91df9010df0
Michael J. Ford <michael.ford@slashetc.us>
2021-05-30 22:10:44 -0400

    ok... should be good now...

--- opt/kc1mjp-tools/bin/cloudlogBackup.sh
+++ opt/kc1mjp-tools/bin/cloudlogBackup.sh
@@ -15,6 +15,8 @@
 #     - fixed case statement (still greasing the gears)
 #     - added -C option for crontab use, updated crontab ask to use
 #     - fixed case statement... again... still greasing....
+#     - yep... need more grease...
+#     - added some more verbosity
 #
 #------------------------------------------------------------------------------
 # --- Script Config
@@ -32,9 +34,9 @@
    fi
 
    crontab=false
-   while getopts :C
+   while getopts :C option
    do
-      case ${opt} in
+      case ${option} in
          C)    crontab=true
                ;;
          \?)   echo "Invalid option: -${OPTARG}"
@@ -66,38 +68,40 @@
 
    for backupFile in ${cloudlogFiles[*]}
    do
-      wget --quiet ${cloudlogBackupUrl}/${backupFile} -o /tmp/cloudlog_${backupFile}
+      echo "INFO: Grabbing ${cloudlogBackupUrl}/${backupFile}"
+      wget --quiet ${cloudlogBackupUrl}/${backupFile} -O /tmp/cloudlog_${backupFile}
       if [[ ! -f /tmp/cloudlog_${backupFile} ]]
       then
          echo "ERROR: Failed to download ${backupFile}"
          exit 1
       fi
 
-      md5New="$( md5sum /tmp/cloudlog_${backupFile} 2>/dev/null )"
-      md5Old="$( md5sum ${destinationPath}/cloudlog_${backupFile} 2>/dev/null )"
+      md5New="$( md5sum /tmp/cloudlog_${backupFile} | awk '{ print $1 }' 2>/dev/null )"
+      md5Old="$( md5sum ${destinationPath}/cloudlog_${backupFile} | awk '{ print $1 }' 2>/dev/null )"
+      echo "INFO: Comparing File - ${backupFile}"
       if [[ ${md5New} == "${md5Old}" ]]
       then
          echo "INFO: ${backupFile} exists"
       else
-         cp /tmp/cloudlog_${backupFile} ${destinationPath}/cloudlog_${backupFile}
+         echo "INFO: Copying /tmp/cloudlog_${backupFile} to ${destinationPath}/cloudlog_${backupFile}"
+         cp -pv /tmp/cloudlog_${backupFile} ${destinationPath}/cloudlog_${backupFile}
       fi
+      echo "INFO: Removing temp file: ${backupFile}"
+      rm /tmp/cloudlog_${backupFile}
    done
 
-   rm /tmp/cloudlog_${backupFile}
-
-   if [[ ${crontab} != true ]]
+   if [[ ${crontab} != true ]] && ! crontab -l | grep -q cloudlogBackup.sh
    then
       echo -en "Enter hourly task into ${USER} crontab? y/N: "
       read answer junk
       case ${answer} in
-         y|Y)  if ! crontab -l | grep -q cloudlogBackup.sh
-               then
-                  echo "0 * * * * /opt/kc1mjp-tools/bin/cloudlogBackup.sh -C &>/dev/null" | crontab -
-               else
-                  echo "INFO: Crontab Exists"
-               fi ;;
-         n|N)  echo "No Crontab Changed"  ;;
-         *)    echo "ERR Invalid Input"   ;;
+         y|Y)  echo "INFO: Inserting Crontab"
+               echo "0 * * * * /opt/kc1mjp-tools/bin/cloudlogBackup.sh -C &>/dev/null" | crontab -
+               ;;
+         n|N)  echo "No Crontab Changed"
+               ;;
+         *)    echo "ERR Invalid Input"
+               ;;
       esac
    fi
 
