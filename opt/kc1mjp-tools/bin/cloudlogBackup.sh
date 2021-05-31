75d11ca21d018cf936f0e4da094f1f49c0b93903
Michael J. Ford <michael.ford@slashetc.us>
2021-05-30 21:45:23 -0400

    fixed my tom foolery

--- opt/kc1mjp-tools/bin/cloudlogBackup.sh
+++ opt/kc1mjp-tools/bin/cloudlogBackup.sh
@@ -13,6 +13,8 @@
 # LastMod: 20210530 - Michael J. Ford <michael.ford@slashetc.us>
 #     - added more reliable download check
 #     - fixed case statement (still greasing the gears)
+#     - added -C option for crontab use, updated crontab ask to use
+#     - fixed case statement... again... still greasing....
 #
 #------------------------------------------------------------------------------
 # --- Script Config
@@ -29,13 +31,26 @@
       exit 1
    fi
 
+   crontab=false
+   while getopts :C
+   do
+      case ${opt} in
+         C)    crontab=true
+               ;;
+         \?)   echo "Invalid option: -${OPTARG}"
+               exit 88
+               ;;
+      esac
+   done
+
 #------------------------------------------------------------------------------
 # --- Main Code
 #------------------------------------------------------------------------------
 
    if ! checkInternet
    then
-      if ! ping -c2 -w2 ${cloudlogHost} &>/dev/null
+      ${useTls} && port=443 || port=80
+      if ! nc -z -w2 ${cloudlogHost} ${port} &>/dev/null
       then
          echo "FATAL: No path to ${cloudlogHost}"
          exit 1
@@ -70,18 +85,21 @@
 
    rm /tmp/cloudlog_${backupFile}
 
-   echo -en "Enter hourly task into ${USER} crontab? y/N: "
-   read answer junk
-   case answer in
-      y|Y)   if ! crontab -l | grep -q cloudlogBackup.sh
-            then
-               echo "0 * * * * /opt/kc1mjp-tools/bin/cloudlogBackup.sh &>/dev/null" | crontab -
-            else
-               echo "INFO: Crontab Exists"
-            fi ;;
-      n|N)   echo "No Crontab Changed"  ;;
-      *)    echo "ERR Invalid Input"   ;;
-   esac
+   if [[ ${crontab} != true ]]
+   then
+      echo -en "Enter hourly task into ${USER} crontab? y/N: "
+      read answer junk
+      case ${answer} in
+         y|Y)  if ! crontab -l | grep -q cloudlogBackup.sh
+               then
+                  echo "0 * * * * /opt/kc1mjp-tools/bin/cloudlogBackup.sh -C &>/dev/null" | crontab -
+               else
+                  echo "INFO: Crontab Exists"
+               fi ;;
+         n|N)  echo "No Crontab Changed"  ;;
+         *)    echo "ERR Invalid Input"   ;;
+      esac
+   fi
 
    echo "INFO: done"
 
